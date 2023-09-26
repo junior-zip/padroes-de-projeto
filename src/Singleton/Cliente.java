07ca8e31c00643c33bcd2c69961c36a2e1b4269b
junior-zip <junior26bmx@gmail.com>
2023-09-26 20:20:36 -0300

    create Singleton

--- src/Singleton/Cliente.java
+++ src/Singleton/Cliente.java
@@ -1,2 +1,18 @@
-package Singleton;public class Cliente {
+package Singleton;
+
+public class Cliente {
+
+
+    public static void main(String[] args) {
+
+
+        LogSingleton logSingleton = LogSingleton.getInstance("Pessoa");
+        LogSingleton logSingleton1 = LogSingleton.getInstance("Animal");
+
+        System.out.println(logSingleton.log);
+        System.out.println(logSingleton1.log);
+
+    }
+
+
 }
--- src/Singleton/LogSingleton.java
+++ src/Singleton/LogSingleton.java
@@ -1,2 +1,33 @@
-package Singleton;public class LogSingleton {
+package Singleton;
+
+public class LogSingleton {
+
+    private static LogSingleton instance;
+    public String log;
+
+
+    private LogSingleton(String Novolog){
+        this.log = Novolog;
+
+    }
+
+    public static LogSingleton getInstance(String Novolog){
+
+        if(instance == null){
+            instance = new LogSingleton(Novolog);
+        }
+        return instance;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
